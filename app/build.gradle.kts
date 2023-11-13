ff0d72a715fcad13d37952289fffb5ec0db31890
Saritzia <saralopez0988@gmail.com>
2023-11-13 01:23:26 +0100

    Activity done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.saritzia.calculator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.saritzia.calculator"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    viewBinding {
+        enable = true
     }
 }
 
--- app/src/main/java/com/saritzia/calculator/MainActivity.kt
+++ app/src/main/java/com/saritzia/calculator/MainActivity.kt
@@ -2,10 +2,26 @@ package com.saritzia.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saritzia.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var result: String
+    private var firstNumber = 0
+    private var secondNumber = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        result = getString(R.string.zero)
+        binding.resultTextView.text = result
+    }
+
+    private fun updateResultText() {
+        binding.resultTextView.text = "$result"
+    }
+
+    private fun checkButtonTapped() {
+        //TO do
     }
 }
